a3991594a50871a75dc95e66052d64a55f5cfd2f
Viktor Dick <vd@perfact.de>
2022-07-25 11:22:22 +0200

    Revert to calculating terms individually, negative numbers are actually not a problem

--- picalc.rs
+++ picalc.rs
@@ -2,19 +2,20 @@ use std::ops::DivAssign;
 use std::cmp::{min,max};
 use std::thread;
 use crossbeam::{channel::{unbounded,Receiver,Sender}};
+use std::time::Instant;
 
-const DIGITS: usize = 100_000;
+const DIGITS: usize = 20_000;
 type Digit = u64;
 type Double = u128;
 
-const MAXTHREADS: usize = 8;
+const MAXTHREADS: usize = 7;
 
 /*
- * Number represents a positive number between 0 (incl.) and 1 (excl.). It uses fixed precision
+ * Number represents a number between -0.5 (incl.) and 0.5 (excl.). It uses fixed precision
  * with DIGITS digits, each of base 2^64. For DIGITS = 10_000, this means 160_000 hexadecimal or
  * 640_000 binary digits. We only implement methods needed for the algorithm, which includes
- * division by a small (64 bit) unsigned integer, addition and subtraction of Numbers and
- * multiplication by 4.
+ * a) addition and subtraction and
+ * b) multiplication by 4 and division by a small (u64) number (only for positiv Numbers.
  */
 #[derive(Clone)]
 struct Number {
@@ -53,12 +54,13 @@ impl Number {
         self.zeros = rhs.zeros;
     }
 
-    //fn set_zero(&mut self) {
-    //    for i in 0..DIGITS {
-    //        self.digits[i] = 0;
-    //    }
-    //    self.zeros = DIGITS;
-    //}
+    #[allow(dead_code)]
+    fn set_zero(&mut self) {
+        for i in 0..DIGITS {
+            self.digits[i] = 0;
+        }
+        self.zeros = DIGITS;
+    }
 
     fn update_zeros_min(&mut self, min: usize) {
         self.zeros = DIGITS;
@@ -135,6 +137,7 @@ impl Number {
         self.update_zeros();
     }
 
+    #[allow(dead_code)]
     fn print(&self) {
         // Print Number as hexadecimal
         for i in 0..DIGITS {
@@ -198,83 +201,60 @@ impl Term {
     }
 }
 
-struct Thread {
-    result: Number,
-    tmp: Number,
-    x2: Digit,
-    snd: Sender<Term>,
-}
-
-impl Thread {
-    fn step(&mut self, mut denom: Digit, mut t1: Term, mut t2: Term) {
-        t1.update(self.x2, denom);
-        self.tmp.set_to_div(&t1.val, denom);
-        // we ignore the value that t2 came with, t1 is closer to the target denom
-        t2.copy_from(&t1);
-        self.snd.send(t1).unwrap();
-        self.result.add_assign(&self.tmp);
-
-        denom += 2;
-        t2.update(self.x2, denom);
-        self.tmp.set_to_div(&t2.val, denom);
-        self.snd.send(t2).unwrap();
-        self.result.sub_assign(&self.tmp);
-    }
-}
-
-fn calc(x2: Digit, rcv: Receiver<(Digit, Term, Term)>, snd: Sender<Term>,
+fn calc(x2: Digit, rcv: Receiver<(bool, Digit, Term)>, snd: Sender<Term>,
         snd_res: Sender<Number>) {
-    // Worker loop. Collect result by iteratively receiving two terms, updating them as necessary
-    // to compute 1/dx^d - 1/(d+2)x^(d+2), which is added to the result. Send the terms back
-    // separately as soon as possible so the intermediate result can more quickly be used by
-    // another thread.
-    // Send the final result back to the main thread once the loop is done.
-    let mut me = Thread {
-        result: Number::zero(),
-        tmp: Number::zero(),
-        x2: x2,
-        snd: snd,
-    };
+    // Worker thread. Iteratively receive a term and which denominator is to be calculated,
+    // update the Term to the value 1/x^d and add or subtract 1/(dx^d) to the result. Once no more
+    // terms are received, pass the result to the main thread, which sums them together.
+    let mut result = Number::zero();
+    let mut tmp = Number::zero();
     loop {
-        let (denom, t1, t2) = match rcv.recv() {
+        let (neg, denom, mut term) = match rcv.recv() {
             Ok(x) => x,
             Err(_) => break,
         };
-        if t1.denom > t2.denom {
-            me.step(denom, t1, t2);
+        term.update(x2, denom);
+        tmp.set_to_div(&term.val, denom);
+        snd.send(term).unwrap();
+        if neg {
+            result.sub_assign(&tmp);
         } else {
-            me.step(denom, t2, t1);
-        }
+            result.add_assign(&tmp);
+        };
     }
-    snd_res.send(me.result).unwrap();
+    snd_res.send(result).unwrap();
 }
 
 fn ataninv(x: Digit) -> Number {
     // Calculate atan(1/x) using Taylor expansion
     let mut result = Number::from_inv(x);
     let x2 = x*x;
-    let mut denom: Digit = 3;
+    let mut denom: Digit = 1;
+    let mut negative = false;
     let mut running = 0;
     // We always store a copy of the val of the completed Term with the highest denominator,
     // so we can push Terms to this denominator before they are started again, so the jump is
-    // not too large - the reason being that we need to divide the val by x2^(diff of denoms),
-    // which might become too large for u64.
-    let mut refdenom = 1;
-    let mut refval = Number::zero();
-    refval.set(&result);
+    // not too large - the reason being that we need to divide the val by x2^(diff of denoms)
+    // and the fact that this needs to fit into u64 is a restriction on the number of terms that
+    // can be handled at the same time.
+    let mut refterm = Term {
+        denom: 1,
+        val: result.clone(),
+    };
 
     let (snd_main, rcv_thrd) = unbounded();
     let (snd_thrd, rcv_main) = unbounded();
     let (snd_res, rcv_res) = unbounded();
 
     for _ in 0..MAXTHREADS+1 {
-        snd_main.send((denom, Term::init(&result), Term::init(&result))).unwrap();
+        negative = !negative;
+        denom += 2;
+        snd_main.send((negative, denom, Term::init(&result))).unwrap();
         running += 1;
-        denom += 4;
 
         // If the number of threads requires a too large jump in the divisor, we break even though
         // we might use more threads.
-        match x2.checked_pow(((denom-1)/2) as u32) {
+        match x2.checked_pow(((denom+1)/2) as u32) {
             Some(_) => (),
             None => break
         }
@@ -293,17 +273,14 @@ fn ataninv(x: Digit) -> Number {
     drop(rcv_thrd);
     drop(snd_thrd);
     drop(snd_res);
-    running *= 2;
 
     let mut refoutput = 1000;
-
-    let mut buffer: Option<Term> = None;
     for mut term in rcv_main {
-        running -= 1;
         if term.val.zeros >= refoutput {
             refoutput += 1000;
         }
         if term.val.is_zero() {
+            running -= 1;
             if running == 0 {
                 drop(snd_main);
                 break;
@@ -311,60 +288,61 @@ fn ataninv(x: Digit) -> Number {
             continue;
         }
 
-        if term.denom > refdenom {
-            refval.set(&term.val);
-            refdenom = term.denom;
-        }
-        if refdenom > term.denom {
-            term.val.set(&refval);
-            term.denom = refdenom;
-        }
-        if buffer.is_some() {
-            snd_main.send((denom, buffer.take().unwrap(), term)).unwrap();
-            denom += 4;
-            running += 2;
+        if term.denom > refterm.denom {
+            refterm.copy_from(&term);
         } else {
-            buffer.get_or_insert(term);
+            term.copy_from(&refterm);
         }
+        negative = !negative;
+        denom += 2;
+        snd_main.send((negative, denom, term)).unwrap();
     }
     for val in rcv_res {
-        result.sub_assign(&val);
+        result.add_assign(&val);
     }
+    println!("{}", denom);
     result
 }
 
-//fn ataninv(x: Digit) -> Number {
-//    let mut result = Number::from_inv(x);
-//    let x2 = x*x;
-//    let mut val = result.clone();
-//    let mut tmp = Number::zero();
-//    let mut denom = 1;
-//    let starttime = Instant::now();
-//    let mut iters = 0;
-//    while !val.is_zero() {
-//        denom += 2;
-//        val /= x2;
-//        tmp.set_to_div(&val, denom);
-//        result.sub_assign(&tmp);
+#[allow(dead_code)]
+fn ataninv_scalar(x: Digit) -> Number {
+    let mut result = Number::from_inv(x);
+    let x2 = x*x;
+    let mut val = result.clone();
+    let mut tmp = Number::zero();
+    let mut denom = 1;
+    let starttime = Instant::now();
+    let mut iters = 0;
+    while !val.is_zero() {
+        denom += 2;
+        val /= x2;
+        tmp.set_to_div(&val, denom);
+        result.sub_assign(&tmp);
 
-//        denom += 2;
-//        val /= x2;
-//        tmp.set_to_div(&val, denom);
-//        result.add_assign(&tmp);
-//        iters += 1;
-//    }
-//    let elapsed = starttime.elapsed();
-//    println!("{} {}.{}", iters, elapsed.as_secs(), elapsed.subsec_millis());
-//    result
-//}
+        denom += 2;
+        val /= x2;
+        tmp.set_to_div(&val, denom);
+        result.add_assign(&tmp);
+        iters += 1;
+    }
+    let elapsed = starttime.elapsed();
+    println!("{} {}.{}", iters, elapsed.as_secs(), elapsed.subsec_millis());
+    result
+}
 
 fn main() {
     // Calculate pi using pi/4 = 4atan(1/5)-atan(1/239)
+    let mut start = Instant::now();
     let mut pi = ataninv(5);
     pi.mul4();
+    let t1 = start.elapsed();
+    start = Instant::now();
     pi.sub_assign(&ataninv(239));
+    let t2 = start.elapsed();
     // Note that this takes the number outside the representable range by creating a value larger
     // than one, which overflows and drops the integer part, but that one is known to be 3.
     pi.mul4();
-    pi.print();
+    //pi.print();
+    println!("{}.{:03} {}.{:03}",
+             t1.as_secs(), t1.subsec_millis(), t2.as_secs(), t2.subsec_millis());
 }
